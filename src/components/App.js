6f036e9252c5edd264d756f9bf54cf29adfa98ef
+mitchellengetich <+mitchellngetich24@gmail.com>
2025-04-09 14:11:37 +0300

    make changes on tests and components

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,35 @@
 import React from "react";
+import { username, city, image } from "../data/user";
+
+
+function Home() {
+  return (
+    <div id="home">
+      <h1>
+        {username} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I Made This</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
+}
+
+function NavBar() {
+  return (
+    <nav>
+      <a href="#home">I'm a link!</a>
+    </nav>
+  );
+}
+
 
 function App() {
   return (
